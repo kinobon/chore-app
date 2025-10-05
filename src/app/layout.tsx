d3d405e48d8389c65fd921c8001b63d291e314d1
kinobon2021 <kinobon2021@gmail.com>
2025-10-06 02:55:58 +0900

    Add iOS PWA support with splash screens and touch icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,73 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   appleWebApp: {
     capable: true,
-    statusBarStyle: "default",
+    statusBarStyle: "black-translucent",
     title: APP_DEFAULT_TITLE,
-    // startUpImage: [],
+    startupImage: [
+      // iPad Pro 12.9"
+      {
+        url: "/apple-splash-2048-2732.jpg",
+        media: "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      // iPad Pro 11"
+      {
+        url: "/apple-splash-1668-2388.jpg",
+        media: "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      // iPad Air, iPad mini
+      {
+        url: "/apple-splash-1536-2048.jpg",
+        media: "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      // iPhone 14 Plus, 15 Plus
+      {
+        url: "/apple-splash-1284-2778.jpg",
+        media: "(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      // iPhone 14, 15, 13, 13 Pro, 12, 12 Pro
+      {
+        url: "/apple-splash-1170-2532.jpg",
+        media: "(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      // iPhone 13 mini, 12 mini, 11 Pro, X, XS
+      {
+        url: "/apple-splash-1125-2436.jpg",
+        media: "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      // iPhone 8, 7, 6s, 6, SE
+      {
+        url: "/apple-splash-750-1334.jpg",
+        media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      // iPhone 14 Pro Max, 15 Pro Max
+      {
+        url: "/apple-splash-1290-2796.jpg",
+        media: "(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      // iPhone 14 Pro, 15 Pro
+      {
+        url: "/apple-splash-1179-2556.jpg",
+        media: "(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      // iPhone 11 Pro Max, XS Max
+      {
+        url: "/apple-splash-1242-2688.jpg",
+        media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)",
+      },
+      // iPhone 11, XR
+      {
+        url: "/apple-splash-828-1792.jpg",
+        media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)",
+      },
+      // iPhone 8 Plus, 7 Plus, 6s Plus, 6 Plus
+      {
+        url: "/apple-splash-1242-2208.jpg",
+        media: "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)",
+      },
+    ],
+  },
+  icons: {
+    apple: "/apple-icon-180.png",
   },
   formatDetection: {
     telephone: false,

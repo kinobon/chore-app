8680ac00397030e1315047517f7aea9a9050ded8
kinobon2021 <kinobon2021@gmail.com>
2025-10-07 02:35:37 +0900

    chore: Update GitHub Actions workflow for Vite and set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/chore-app/" : "/",
   plugins: [
     react({
       babel: {
